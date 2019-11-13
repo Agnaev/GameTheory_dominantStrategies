a531bbb89e6382b6a58cddc6e1a0e41d74a7739f
agnaev <agnev@mwpost.ru>
2019-11-13 23:13:01 +0300

    compression code

--- Program.cs
+++ Program.cs
@@ -1,72 +1,43 @@
 ﻿using System;
 
-namespace laba_2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double[,] mas = new double[,] {
-                { 2, 7, 5, 8, 4 },
-                { 1, 6, 4, 7, 3 },
-                { 3, 8, 6, 9, 5 },
-                { 5, 3, 7, 9, 5 }
-            };
+namespace laba_2 {
+    public class Program {
+        public static void Main(string[] args) {
+            Console.Write("Введите Размерность матрицы\nx = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y = ");
+            int y = Convert.ToInt32(Console.ReadLine());
 
-            //double[,] mas = new double[,]
-            //{
-            //    { 4, 5, 6, 7},
-            //    { 3, 4, 5, 5},
-            //    { 7, 6, 10, 8},
-            //    { 8, 5, 4, 3}
-            //};
-
-            //double[,] mas = new double[,]
-            //{
-            //    { 4, 8, 1, 12 },
-            //    { 4, 11, 3, 7 },
-            //    { 6, 9, 5, 10 },
-            //    { 7, 14, 4, 5 },
-            //    { 8, 16, 2, 20 }
-            //};
-
-            Kernel kernel = new Kernel(mas);
-            Console.WriteLine();
+            double[,] mas = new double[x, y];
+            Console.WriteLine("Введите матрицу через пробелы");
+            for (int i = 0; i < x; i++) {
+                var tmp = Console.ReadLine().Replace(".", ",").Replace("  ", " ").Split(' ');
+                for (int j = 0; j < tmp.Length && j < y; j++)
+                    double.TryParse(tmp[j], out mas[i, j]);
+            }
+            Kernel beforeChange, kernel = new Kernel(mas);
             kernel.Print();
-            Console.WriteLine();
-            Kernel beforeChange;
             do {
                 beforeChange = kernel;
-                for (int i = 0; i < mas.GetLength(0); i++)
-                {
-                    for (int j = 0; j < mas.GetLength(0); j++)
-                    {
-                        if(i == j)
-                        {
+                for (int i = 0; i < mas.GetLength(0); i++) {
+                    for (int j = 0; j < mas.GetLength(0); j++) {
+                        if(i == j) {
                             continue;
                         }
-                        if (kernel.StrategyMatrix[i] <= kernel.StrategyMatrix[j])
-                        {
+                        if (kernel.StrategyMatrix[i] <= kernel.StrategyMatrix[j]) {
                             kernel.RemoveStrategy(i);
                         }
                     }
                 }
                 kernel = Kernel.Transpose(kernel);
                 kernel = new Kernel(Kernel.ToDouble(kernel));
-                //Console.WriteLine();
-                //kernel.Print();
-                //Console.WriteLine();
-                for (int i = 0; i < mas.GetLength(1); i++)
-                {
-                    for (int j = 0; j < mas.GetLength(1); j++)
-                    {
-                        if(i == j)
-                        {
+                for (int i = 0; i < mas.GetLength(1); i++) {
+                    for (int j = 0; j < mas.GetLength(1); j++) {
+                        if(i == j) {
                             continue;
                         }
 
-                        if (kernel.StrategyMatrix[i] >= kernel.StrategyMatrix[j])
-                        {
+                        if (kernel.StrategyMatrix[i] >= kernel.StrategyMatrix[j]) {
                             kernel.RemoveStrategy(i);
                         }
                     }
--- kernel.cs
+++ kernel.cs
@@ -8,271 +8,135 @@ namespace laba_2
     internal class Strategy
     {
         public List<double> strategy = new List<double>();
-
         public double this[int index]
         {
-            get
-            {
-                return strategy[index];
-            }
-            set
-            {
-                strategy[index] = value;
-            }
+            get => strategy[index];
+            set => strategy[index] = value;
         }
-
-        public Strategy(double[] mas)
-        {
-            foreach(var i in mas)
-            {
-                strategy.Add(i);
-            }
-        }
-
+        public Strategy(double[] mas) => mas.ToList().ForEach(item => strategy.Add(item));
         public static bool operator ==(Strategy a, Strategy b)
         {
             if (a.strategy.Count != b.strategy.Count)
-            {
                 return false;
-            }
-
             for(int i = 0; i < a.strategy.Count; i++)
-            {
                 if(a.strategy[i] != b.strategy[i])
-                {
                     return false;
-                }
-            }
             return true;
         }
-
         public static bool operator != (Strategy a, Strategy b)
         {
             if(a.strategy.Count != b.strategy.Count)
-            {
                 return false;
-            }
-
             for(int i = 0; i < a.strategy.Count; i++)
-            {
                 if(a.strategy[i] != b.strategy[i])
-                {
                     return true;
-                }
-            }
             return false;
         }
-
         public static bool operator <=(Strategy a, Strategy b)
         {
             if(a.strategy.Count != b.strategy.Count)
-            {
                 return false;
-            }
-
             if(a.strategy.All(x => x == double.MaxValue) || b.strategy.All(x => x == double.MaxValue))
-            {
                 return false;
-            }
-
             for(int i = 0; i < a.strategy.Count; i++)
-            {
                 if(a.strategy[i] > b.strategy[i])
-                {
                     return false;
-                }
-            }
             return true;
         }
-
         public static bool operator >= (Strategy a, Strategy b)
         {
             if(a.strategy.Count != b.strategy.Count)
-            {
-                return false;
-            }
+                    return false;
 
             if(a.strategy.All(x => x == double.MaxValue) || b.strategy.All(x => x == double.MaxValue))
-            {
                 return false;
-            }
 
             for(int i = 0; i <a.strategy.Count; i++)
-            {
                 if(a.strategy[i] < b.strategy[i])
-                {
                     return false;
-                }
-            }
             return true;
         }
-
         public static bool operator <(Strategy a, Strategy b)
         {
             if(a.strategy.Count != b.strategy.Count)
-            {
                 return false;
-            }
-
             if (a.strategy.All(x => x == double.MaxValue) || b.strategy.All(x => x == double.MaxValue))
-            {
                 return false;
-            }
-
             for (int i = 0; i < a.strategy.Count; i++)
-            {
                 if(a.strategy[i] >= b.strategy[i])
-                {
                     return false;
-                }
-            }
             return true;
         }
-
         public static bool operator >(Strategy a, Strategy b)
         {
             if(a.strategy.Count != b.strategy.Count)
-            {
                 return false;
-            }
-
             if (a.strategy.All(x => x == double.MaxValue) || b.strategy.All(x => x == double.MaxValue))
-            {
                 return false;
-            }
-
             for (int i = 0; i < a.strategy.Count; i++)
-            {
                 if(a.strategy[i] <= b.strategy[i])
-                {
                     return false;
-                }
-            }
             return true;
         }
-
         public void Print()
         {
-            strategy.ForEach(i =>
-            {
+            strategy.ForEach(i => {
                 Console.Write(i == double.MaxValue ? " 0 " : $" {i} ");
             });
             Console.WriteLine();
         }
     }
 
-    internal class Kernel
-    {
+    internal class Kernel {
         public List<Strategy> StrategyMatrix = new List<Strategy>();
-
-        public override bool Equals(object obj)
-        {
-            return StrategyMatrix == obj as List<Strategy>;//base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
-        }
-
-        public Kernel(double[,] mas)
-        {
-            for(int i = 0; i < mas.GetLength(0); i++)
-            {
+        public Kernel(double[,] mas) {
+            for(int i = 0; i < mas.GetLength(0); i++) {
                 double[] tmp = new double[mas.GetLength(1)];
                 for(int j = 0; j < mas.GetLength(1); j++)
-                {
                     tmp[j] = mas[i, j];
-                }
                 StrategyMatrix.Add(new Strategy(tmp));
             }
         }
-
         public void Print() => StrategyMatrix.ForEach(strategy => strategy.Print());
-
-        public static double[,] ToDouble(Kernel kernel)
-        {
+        public static double[,] ToDouble(Kernel kernel) {
             double[,] result = new double[kernel.StrategyMatrix.Count, Convert.ToInt32(kernel.StrategyMatrix.Max(x => x.strategy.Count))];
             for(int i = 0; i < result.GetLength(0); i++)
-            {
                 for(int j = 0; j < result.GetLength(1); j++)
-                {
                     result[i, j] = kernel.StrategyMatrix[i][j];
-                }
-            }
             return result;
         }
-
         public static Kernel Transpose(Kernel k)
         {
             double[,] result = new double[k.StrategyMatrix.Max(x => x.strategy.Count), k.StrategyMatrix.Count];
             for(int i = 0; i < k.StrategyMatrix.Count(); i++)
-            {
                 for(int j = 0; j < k.StrategyMatrix[i].strategy.Count; j++)
-                {
                     result[j, i] = k.StrategyMatrix[i].strategy[j];
-                }
-            }
             return new Kernel(result);
         }
-
-        public void RemoveStrategy(int line)
-        {
+        public void RemoveStrategy(int line) {
             for(int i = 0; i < StrategyMatrix[line].strategy.Count; i++)
-            {
                 StrategyMatrix[line].strategy[i] = double.MaxValue;
-            }
         }
-
-        public Strategy this[int index]
-        {
-            get
-            {
-                return StrategyMatrix[index];
-            }
-            set
-            {
-                StrategyMatrix[index] = value;
-            }
+        public Strategy this[int index] {
+            get => StrategyMatrix[index]; 
+            set =>  StrategyMatrix[index] = value; 
         }
-
-        public static bool operator == (Kernel a, Kernel b)
-        {
+        public static bool operator == (Kernel a, Kernel b) {
             if (a.StrategyMatrix.Count != b.StrategyMatrix.Count)
-            {
                 return false;
-            }
-
             for (int i = 0; i < a.StrategyMatrix.Count; i++)
-            {
                 for(int j = 0; j < a.StrategyMatrix[i].strategy.Count; j++)
-                {
                     if(a[i].strategy[j] != b.StrategyMatrix[i].strategy[j])
-                    {
                         return false;
-                    }
-                }
-            }
             return true;
         }
-
-        public static bool operator != (Kernel a, Kernel b)
-        {
+        public static bool operator != (Kernel a, Kernel b) {
             if(a.StrategyMatrix.Count != b.StrategyMatrix.Count)
-            {
                 return false;
-            }
-
             for (int i = 0; i < a.StrategyMatrix.Count; i++)
-            {
                 for (int j = 0; j < a.StrategyMatrix[i].strategy.Count; j++)
-                {
                     if (a[i].strategy[j] != b[i].strategy[j])
-                    {
                         return true;
-                    }
-                }
-            }
             return false;
         }
     }
